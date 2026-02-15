6267c7b3275995c0859aa15832203d7323b005f9
MaheshAcharya <azotatoza@gmail.com>
2026-02-15 15:17:22 +0545

    footer

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -22,13 +22,19 @@ const Header = () => {
       {user?.role === "admin" && (
         <div className="w-64 bg-gray-700 text-white shrink-0">
           <h2 className="text-2xl font-bold p-6">Admin Panel</h2>
-          <NavLink to="/dashboard" className="block py-2 px-6 hover:text-blue-300">
+          <NavLink
+            to="/dashboard"
+            className="block py-2 px-6 hover:text-blue-300"
+          >
             Dashboard
           </NavLink>
-          <NavLink to="/viewblogs" className="block py-2 px-6 hover:text-blue-300">
+          <NavLink
+            to="/viewblogs"
+            className="block py-2 px-6 hover:text-blue-300"
+          >
             Manage Blogs
           </NavLink>
-          
+
           <NavLink to="/liked" className="block py-2 px-6 hover:text-blue-300">
             Liked Blogs
           </NavLink>
@@ -38,42 +44,88 @@ const Header = () => {
       {/* Main Content */}
       <div className="flex-1 flex flex-col">
         {/* Header */}
-        <div className="bg-slate-300 flex justify-between items-center p-4">
-          <div
-            className="text-2xl font-bold text-indigo-600 cursor-pointer"
-            onClick={() => navigate("/")}
-          >
-            MyBlogApp
-          </div>
+        <div className="bg-slate-300 flex justify-between items-center px-10 h-20 shadow-md">
+        <div
+  onClick={() => navigate("/")}
+  className="cursor-pointer flex  leading-tight"
+>
+  <span className="text-2xl font-semibold tracking-wide 
+    bg-linear-to-r from-indigo-600 via-blue-500 to-emerald-500 
+    bg-clip-text text-transparent">
+    Explore
+  </span>
 
-          <nav className="flex items-center gap-4">
-            <NavLink to="/" className="px-4 py-2 rounded-md">
-              Home
-            </NavLink>
-            <NavLink to="/viewblogs" className="px-4 py-2 rounded-md">
-              Blog
-            </NavLink>
-            <NavLink to="/liked" className="px-4 py-2 rounded-md">
-              Liked Blogs
-            </NavLink>
-            <NavLink to="/about" className="px-4 py-2 rounded-md">
-              About Us
-            </NavLink>
-            <NavLink to="/contact" className="px-4 py-2 rounded-md">
-              Contact Us
-            </NavLink>
+  <span className="text-2xl font-extrabold tracking-widest
+     text-green-700">
+    NEPAL
+  </span>
+</div>
+          {/* Navigation */}
+          <nav className="flex items-center gap-8 text-1rem font-medium">
+            <NavLink
+  to="/"
+  className={({ isActive }) =>
+    isActive
+      ? "text-blue-600 font-bold underline"
+      : "text-gray-800 hover:text-blue-500"
+  }
+>
+  Home
+</NavLink>
+
+<NavLink
+  to="/viewblogs"
+  className={({ isActive }) =>
+    isActive
+      ? "text-blue-600 font-bold underline"
+      : "text-gray-800 hover:text-blue-500"
+  }
+>
+  Blog
+</NavLink>
+
+<NavLink
+  to="/liked"
+  className={({ isActive }) =>
+    isActive
+      ? "text-blue-600 font-bold underline"
+      : "text-gray-800 hover:text-blue-500"
+  }
+>
+  Liked Blogs
+</NavLink>
+
+<NavLink
+  to="/about"
+  className={({ isActive }) =>
+    isActive
+      ? "text-blue-600 font-bold underline"
+      : "text-gray-800 hover:text-blue-500"
+  }
+>
+  About Us
+</NavLink>
+
+<NavLink
+  to="/contact"
+  className={({ isActive }) =>
+    isActive
+      ? "text-blue-600 font-bold underline"
+      : "text-gray-800 hover:text-blue-500"
+  }
+>
+  Contact Us
+</NavLink>
 
             {isAuthenticated ? (
               <button
                 onClick={handleLogout}
-                className="px-4 py-2 bg-red-600 text-white rounded-md"
+                className="px-4 py-1 bg-red-600 text-white rounded-md"
               >
                 Logout
               </button>
             ) : (
-              <NavLink to="/login" className="px-4 py-2 rounded-md">
-                Login
-              </NavLink>
+              <NavLink to="/login">Login</NavLink>
             )}
           </nav>
         </div>
